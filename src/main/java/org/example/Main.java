56810adcf68cd04cacc033f328170b898cee4458
janderson <jandersonandres29@gmail.com>
2025-08-04 10:21:29 -0500

    Se crea menu de opciones

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        //Deseamos construir un programa por consola
+        //que permita la gestion de pedidos
+        //de una plataforma como rapi
+
+        //un pedido tiene:
+        //1. ID
+        //2. Fecha
+        //3. Lista de productos
+        //4. Direccion de envio
+        //5. Nombre del cliente de envio
+        //6. Nombre del restaurante
+
+        //Necesitamos construir un menu
+        //1. Crear pedido
+        //2. Ver pedido
+        //3. Eliminar pedido
+        //4. Salir
+
+        //ENTRADAS DEL PROBLEMA
+        Scanner leerDato = new Scanner (System.in);
+        Integer controlDelCiclo =100;
+
+        while(controlDelCiclo !=4){
+
+            System.out.println("Apreciado Usuario, digita una opcion:");
+            controlDelCiclo=leerDato.nextInt();
+
+            if(){
+
+            }else if(){
+
+            } else if() {
+
+            }else{
+
+            }
+        }
+
+
+    }
+}
